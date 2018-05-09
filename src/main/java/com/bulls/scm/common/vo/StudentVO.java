44117245a0c7066d24fd3af499ef8f18f6422302
Vinoth <Vinoth@VinoReddy>
2018-05-10 00:42:45 +0530

    export student functionality

---
+++ src/main/java/com/bulls/scm/common/vo/StudentVO.java
@@ -0,0 +1,270 @@
+package com.bulls.scm.common.vo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class StudentVO {
+
+	private int academicYear;
+
+	private Date admissionDate;
+
+	private String admissionNo;
+
+	private BigDecimal alternateMobileNo;
+
+	private int caste;
+
+	private Date dob;
+
+	private String dualMode;
+
+	private String email;
+
+	private String fatherName;
+
+	private String gender;
+
+	private String highestQualification;
+
+	private int id;
+
+	private String identificationMarks;
+
+	private BigDecimal mobileNo;
+
+	private String motherName;
+
+	private String name;
+
+	private int phase;
+
+	private String photoSbmtd;
+
+	private String presentAddress;
+
+	private String registrationNo;
+
+	private String scholarship;
+
+	private String shift;
+
+	private int trade;
+
+	private int type;
+
+	private String UID_Number;
+
+	private String unit;
+
+	public int getAcademicYear() {
+		return academicYear;
+	}
+
+	public void setAcademicYear(int academicYear) {
+		this.academicYear = academicYear;
+	}
+
+	public Date getAdmissionDate() {
+		return admissionDate;
+	}
+
+	public void setAdmissionDate(Date admissionDate) {
+		this.admissionDate = admissionDate;
+	}
+
+	public String getAdmissionNo() {
+		return admissionNo;
+	}
+
+	public void setAdmissionNo(String admissionNo) {
+		this.admissionNo = admissionNo;
+	}
+
+	public BigDecimal getAlternateMobileNo() {
+		return alternateMobileNo;
+	}
+
+	public void setAlternateMobileNo(BigDecimal alternateMobileNo) {
+		this.alternateMobileNo = alternateMobileNo;
+	}
+
+	public int getCaste() {
+		return caste;
+	}
+
+	public void setCaste(int caste) {
+		this.caste = caste;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getDualMode() {
+		return dualMode;
+	}
+
+	public void setDualMode(String dualMode) {
+		this.dualMode = dualMode;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFatherName() {
+		return fatherName;
+	}
+
+	public void setFatherName(String fatherName) {
+		this.fatherName = fatherName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getHighestQualification() {
+		return highestQualification;
+	}
+
+	public void setHighestQualification(String highestQualification) {
+		this.highestQualification = highestQualification;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getIdentificationMarks() {
+		return identificationMarks;
+	}
+
+	public void setIdentificationMarks(String identificationMarks) {
+		this.identificationMarks = identificationMarks;
+	}
+
+	public BigDecimal getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(BigDecimal mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getMotherName() {
+		return motherName;
+	}
+
+	public void setMotherName(String motherName) {
+		this.motherName = motherName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPhase() {
+		return phase;
+	}
+
+	public void setPhase(int phase) {
+		this.phase = phase;
+	}
+
+	public String getPhotoSbmtd() {
+		return photoSbmtd;
+	}
+
+	public void setPhotoSbmtd(String photoSbmtd) {
+		this.photoSbmtd = photoSbmtd;
+	}
+
+	public String getPresentAddress() {
+		return presentAddress;
+	}
+
+	public void setPresentAddress(String presentAddress) {
+		this.presentAddress = presentAddress;
+	}
+
+	public String getRegistrationNo() {
+		return registrationNo;
+	}
+
+	public void setRegistrationNo(String registrationNo) {
+		this.registrationNo = registrationNo;
+	}
+
+	public String getScholarship() {
+		return scholarship;
+	}
+
+	public void setScholarship(String scholarship) {
+		this.scholarship = scholarship;
+	}
+
+	public String getShift() {
+		return shift;
+	}
+
+	public void setShift(String shift) {
+		this.shift = shift;
+	}
+
+	public int getTrade() {
+		return trade;
+	}
+
+	public void setTrade(int trade) {
+		this.trade = trade;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public String getUID_Number() {
+		return UID_Number;
+	}
+
+	public void setUID_Number(String uID_Number) {
+		UID_Number = uID_Number;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	
+	
+	
+}
